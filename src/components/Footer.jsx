bb82933ad6c2f4a65c92692f70eebe7325302816
Prakas-sys <prakashbalayar92@gmail.com>
2025-12-01 10:29:14 +0545

    Force redeploy to apply footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import { Facebook, Instagram, Twitter, TikTok, Mail, Phone, MapPin, Globe } from
 export default function Footer() {
     return (
         <footer className="bg-sky-900 text-white pt-12 pb-24">
+            {/* Updated Contact Info & Social Links */}
             <div className="max-w-7xl mx-auto px-6">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-12">
                     {/* Brand */}
