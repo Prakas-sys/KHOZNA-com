5d88bc749b6142bff6c7df4528658111eea0ec83
Prakas-sys <prakashbalayar92@gmail.com>
2025-12-06 17:36:03 +0545

    Update asset setion

--- add-back-photo-column.sql
+++
@@ -1,3 +0,0 @@
--- Add back side photo column to kyc_verifications table
-ALTER TABLE public.kyc_verifications 
-ADD COLUMN IF NOT EXISTS citizenship_photo_back_url text;
--- add-citizenship-back-column.sql
+++
@@ -1,5 +0,0 @@
--- Add missing citizenship_photo_back_url column to kyc_verifications table
--- Run this in your Supabase SQL Editor
-
-ALTER TABLE public.kyc_verifications 
-ADD COLUMN IF NOT EXISTS citizenship_photo_back_url text;
