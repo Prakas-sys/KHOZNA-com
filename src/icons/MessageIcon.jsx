d2f38cd81576297d5aeb88a54dd916c9fddb6223
Prakas-sys <prakashbalayar92@gmail.com>
2025-12-13 09:34:41 +0545

    Integrate improved MessageIcon from public asset

--- src/icons/MessageIcon.jsx
+++ src/icons/MessageIcon.jsx
@@ -1,17 +1,17 @@
 const MessageIcon = (props) => {
     return (
         <svg
-            viewBox="0 0 24 24"
+            viewBox="0 0 15 15"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             aria-hidden="true"
             stroke="currentColor"
-            strokeWidth="2"
-            strokeLinejoin="round"
+            strokeWidth="1.5" /* Adjusted to match the delicate 15px grid, or can be overridden */
             strokeLinecap="round"
+            strokeLinejoin="round"
             {...props}
         >
-            <path d="M21 12c0 4.418-4.03 8-9 8-1.038 0-2.037-.155-2.964-.44L3 21l1.755-4.318C4.273 15.86 3 14.01 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8Z" />
+            <path d="M5.5 11.493L7.5 14.491L9.5 11.493H13.5C13.7652 11.493 14.0196 11.3876 14.2071 11.2001C14.3946 11.0125 14.5 10.7582 14.5 10.493V1.49998C14.4997 1.23493 14.3943 0.980836 14.2068 0.793516C14.0192 0.606196 13.765 0.500976 13.5 0.500977H1.5C1.23478 0.500977 0.98043 0.606333 0.792893 0.79387C0.605357 0.981406 0.5 1.23576 0.5 1.50098V10.495C0.5 11.047 0.947 11.494 1.5 11.494L5.5 11.493Z" />
         </svg>
     );
 };
